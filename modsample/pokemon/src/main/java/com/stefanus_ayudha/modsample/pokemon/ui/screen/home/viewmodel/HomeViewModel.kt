4c9855dd377d026eff2622df40173447f82b510f
stefanus ayudha <stefanus.ayudha@gmail.com>
2022-11-11 11:47:35 +0700

    simplify state view model creator

--- modsample/pokemon/src/main/java/com/stefanus_ayudha/modsample/pokemon/ui/screen/home/viewmodel/HomeViewModel.kt
+++ modsample/pokemon/src/main/java/com/stefanus_ayudha/modsample/pokemon/ui/screen/home/viewmodel/HomeViewModel.kt
@@ -1,18 +1,17 @@
 package com.stefanus_ayudha.modsample.pokemon.ui.screen.home.viewmodel
 
 import com.stefanus_ayudha.core.common.util.viewmodel.BaseViewModel
+import com.stefanus_ayudha.core.common.util.viewmodel.stateCreator
 import com.stefanus_ayudha.core.network.GetPokemonListQuery
 import com.stefanus_ayudha.modsample.pokemon.data.getPokemonList
 import com.stefanus_ayudha.modsample.pokemon.data.payload.GetPokemonListPld
 
 class HomeViewModel : BaseViewModel(), HomeViewModelUseCase {
 
-    override val pokemonListState = object : State<GetPokemonListQuery.Data, GetPokemonListPld>() {
-        override val operator: suspend (payload: GetPokemonListPld) -> GetPokemonListQuery.Data
-            get() = {
-                getPokemonList(it)
-            }
-    }
+    override val pokemonListState =
+        stateCreator<GetPokemonListQuery.Data, GetPokemonListPld> {
+            getPokemonList(it)
+        }
 
     override fun clear() {
         TODO("Not yet implemented")
